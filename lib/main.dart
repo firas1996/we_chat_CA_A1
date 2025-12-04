321be4a10091a0d871cfa8a11a4ecb5b59233068
firas1996 <firas.hm.1996@gmail.com>
2025-12-04 18:53:11 +0100

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:we_chat/screens/auth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:we_chat/screens/chat.dart';
+import 'package:we_chat/screens/splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,6 +18,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: AuthScreen());
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // return const Center(child: CircularProgressIndicator());
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
+    );
   }
 }
